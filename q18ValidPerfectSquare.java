32a8ad5c2c3654c983e2817fa10201cf7fbe9374
Tanesha Gahtori <iamtanesha13@gmail.com>
2023-06-01 00:36:32 +0530

    18th code

---
+++ q18ValidPerfectSquare.java
@@ -0,0 +1,22 @@
+//Valid perfect square
+
+
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long start=1, end=num;
+        while (start<=end) {
+            long mid= (start+end)/2;
+            if (mid*mid==num){
+                return true;
+            }
+            else if (mid*mid>num) {
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return false;
+        
+    }
+}
