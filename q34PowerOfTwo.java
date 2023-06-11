04b80dc280b1915043bad6078796338ca2285644
Tanesha Gahtori <iamtanesha13@gmail.com>
2023-06-12 03:57:38 +0530

    34th code

---
+++ q34PowerOfTwo.java
@@ -0,0 +1,32 @@
+// class Solution {
+//     public boolean isPowerOfTwo(int n) {
+//         if(n<1) {
+//             return false;
+//         }
+//         if (n==1) {
+//             return true;
+//         }
+//         if(n%2==1) {
+//             return false;
+
+//         }
+//         return isPowerOfTwo(n/2);
+//     }
+// }
+
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        long i=1;
+        while(i<n) {
+            i=i*2;
+        }
+        if (i==n) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+}
