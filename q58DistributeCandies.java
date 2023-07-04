16462a09126dd694ff54fd10b1b49a4307f66059
Tanesha Gahtori <iamtanesha13@gmail.com>
2023-07-05 01:35:05 +0530

    58th code

---
+++ q58DistributeCandies.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+        HashSet <Integer> set= new HashSet<>();
+        for (int i:candyType) {
+            set.add(i);
+        }
+        return Math.min(candyType.length/2, set.size());
+        
+    }
+}
