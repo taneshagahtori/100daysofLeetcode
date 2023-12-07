f1a528de8bb862daa285a95ca28825fa8225a247
Tanesha Gahtori <iamtanesha13@gmail.com>
2023-12-07 18:40:33 +0530

    61th code

--- q61StudentsAndExaminations.java
+++ q61StudentsAndExaminations.java
@@ -1,3 +1,4 @@
+//q61
 select s.Student_id, s.Student_name, u.subject_name, count(e.subject_name) as attended_exams from
 Students as s join Subjects as u left join Examinations as e on
 s.student_id = e.student_id and u.subject_name = e.subject_name
