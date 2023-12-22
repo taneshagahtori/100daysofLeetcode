8c2f117ce6c30a8bedcb0554ba8355e5fa64d35e
taneshagahtori <iamtanesha13@gmail.com>
2023-12-23 01:07:12 +0530

    98th code

---
+++ q98ReverseOnlyLetters.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String reverseOnlyLetters(String s) {
+        char [] c= s.toCharArray();
+        int start=0;
+        int end=s.length()-1;
+
+        while(start<end){
+            if(!isEnglishCharacter(c[start])) {
+                start++;
+                continue;
+            }
+            if(!isEnglishCharacter(c[end])) {
+                end--;
+                continue;
+            }
+
+            char temp= c[start];
+            c[start]=c[end];
+            c[end]= temp;
+            start++;
+            end--;
+        }
+        return new String(c);
+    }
+    private boolean isEnglishCharacter(char c){
+        return Character.isUpperCase(c) || Character.isLowerCase(c);
+    }
+}
